4fc9614b6d0e66b716626960e3988f8da17e1b2f
Luca <l.pelissero.1360@vallauri.edu>
2021-10-06 09:18:24 +0200

    Esercizio 5 - persons aggiunto

---
+++ Node.js/Es 005 - persons/server.ts
@@ -0,0 +1,28 @@
+import * as _http from "http";
+
+let HEADERS = require("./headers.json");
+let dispatcher = require("./dispatcher.ts");
+let port:number = 1337;
+
+
+// tutte le volte che arriva una richgiesta dal client parte questa funzione
+let server = _http.createServer(function (req, res) {
+    dispatcher.dispatch(req, res);
+});
+server.listen(port);
+console.log("Server in ascolto sulla porta "+ port);
+
+// registrazione dei servizi
+dispatcher.addListener("POST", "/api/servizio1", function (req, res) {
+    res.writeHead(200, HEADERS.json);
+    let nome = req["BODY"].nome;
+    res.write(JSON.stringify({"ris": nome, "id": req["GET"].id}));
+    res.end();
+})
+
+dispatcher.addListener("GET", "/api/servizio2", function (req, res) {
+    res.writeHead(200, HEADERS.json);
+    let nome = req["GET"].nome; 
+    res.write(JSON.stringify({"ris":nome}));
+    res.end();
+})
---
+++ Node.js/Es 005 - persons/static/index.js
@@ -0,0 +1,13 @@
+"use strict"
+$(document).ready(function() {
+    let _lstNazioni = $("#lstNazioni");
+    let _tabStudenti = $("#tabStudenti");
+    let _divDettagli = $("#divDettagli");
+
+    _divDettagli.hide()
+
+ 
+ 
+ 
+ 
+})
