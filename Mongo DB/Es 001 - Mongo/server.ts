d0c7dced1a88bc55b04cc97e9aa0cc2ba4497753
Luca <l.pelissero.1360@vallauri.edu>
2021-10-30 17:19:52 +0200

    Updated server

--- Mongo DB/Es 001 - Mongo/server.ts
+++ Mongo DB/Es 001 - Mongo/server.ts
@@ -13,34 +13,34 @@ const server = _http.createServer(function (req, res) {
 server.listen(port);
 console.log("Server in ascolto sulla porta " + port);
 
-// inserimento nuovo record
-mongoClient.connect("mongodb://127.0.0.1:27017", function (err, client) {
-    if (!err) {
-        let db = client.db("5B_Studenti");
-        let collection = db.collection("Studenti");
-        let student = {
-            "nome": "Piepaolo",
-            "cognome": "Pelissero",
-            "indirizzo": "Informatica",
-            "sezione": "B",
-            "hobbies": ["nuoto", "calcio"],
-            "residenta": {"citta":"Genola", "provincia":"Cuneo", "cap":"12045"},
-            "lavoratore": false
-        };
-        collection.insertOne(student,function (err, data) {
-            if (!err) {
-                console.log(data);
-            }
-            else {
-                console.log("Errore esecuzione qury: " + err.message);
-            }
-            client.close();
-        })
-    }
-    else {
-        console.log("Errore nella connessione al database: " + err.message);
-    }
-});
+// inserimento nuovo record (lo commento altrimenti tutte le volte aggiunge un record)
+// mongoClient.connect("mongodb://127.0.0.1:27017", function (err, client) {
+//     if (!err) {
+//         let db = client.db("5B_Studenti");
+//         let collection = db.collection("Studenti");
+//         let student = {
+//             "nome": "Piepaolo",
+//             "cognome": "Pelissero",
+//             "indirizzo": "Informatica",
+//             "sezione": "B",
+//             "hobbies": ["nuoto", "calcio"],
+//             "residenta": {"citta":"Genola", "provincia":"Cuneo", "cap":"12045"},
+//             "lavoratore": false
+//         };
+//         collection.insertOne(student,function (err, data) {
+//             if (!err) {
+//                 console.log(data);
+//             }
+//             else {
+//                 console.log("Errore esecuzione qury: " + err.message);
+//             }
+//             client.close();
+//         })
+//     }
+//     else {
+//         console.log("Errore nella connessione al database: " + err.message);
+//     }
+// });
 
 // modello di accesso al data base
 mongoClient.connect("mongodb://127.0.0.1:27017", function (err, client) {
