429f88b720d23c0abf75c9f0df6302aa81791718
Luca <l.pelissero.1360@vallauri.edu>
2021-11-03 08:17:08 +0100

    Implementazione metodo Delete e Update

--- Mongo DB/Es 001 - Mongo/server.ts
+++ Mongo DB/Es 001 - Mongo/server.ts
@@ -4,17 +4,18 @@ import HEADERS from "./headers.json";
 import { Dispatcher } from "./dispatcher";
 
 const mongoClient = _mongodb.MongoClient;
-const port: number = 1337;
-const dispatcher: Dispatcher = new Dispatcher();
+const CONNECTIONSTRING = "mongodb://127.0.0.1:27017";
+// const port: number = 1337;
+// const dispatcher: Dispatcher = new Dispatcher();
 
-const server = _http.createServer(function (req, res) {
-    dispatcher.dispatch(req, res);
-});
-server.listen(port);
-console.log("Server in ascolto sulla porta " + port);
+// const server = _http.createServer(function (req, res) {
+//     dispatcher.dispatch(req, res);
+// });
+// server.listen(port);
+// console.log("Server in ascolto sulla porta " + port);
 
 // inserimento nuovo record (lo commento altrimenti tutte le volte aggiunge un record)
-// mongoClient.connect("mongodb://127.0.0.1:27017", function (err, client) {
+// mongoClient.connect(CONNECTIONSTRING, function (err, client) {
 //     if (!err) {
 //         let db = client.db("5B_Studenti");
 //         let collection = db.collection("Studenti");
@@ -29,7 +30,7 @@ console.log("Server in ascolto sulla porta " + port);
 //         };
 //         collection.insertOne(student,function (err, data) {
 //             if (!err) {
-//                 console.log(data);
+//                 console.log("Insert: "+data);
 //             }
 //             else {
 //                 console.log("Errore esecuzione qury: " + err.message);
@@ -43,14 +44,54 @@ console.log("Server in ascolto sulla porta " + port);
 // });
 
 // modello di accesso al data base
-mongoClient.connect("mongodb://127.0.0.1:27017", function (err, client) {
+mongoClient.connect(CONNECTIONSTRING, function (err, client) {
     if (!err) {
         let db = client.db("5B_Studenti");
         let collection = db.collection("Studenti");
         // .find() restituisce tutti i record
         collection.find().toArray(function (err, data) {
             if (!err) {
-                console.log(data);
+                console.log("find: " + data);
+            }
+            else {
+                console.log("Errore esecuzione qury: " + err.message);
+            }
+            client.close();
+        });
+    }
+    else {
+        console.log("Errore nella connessione al database: " + err.message);
+    }
+});
+
+// Update (filtro, azione)
+mongoClient.connect(CONNECTIONSTRING, function (err, client) {
+    if (!err) {
+        let db = client.db("5B_Studenti");
+        let collection = db.collection("Studenti");
+        collection.updateOne({ "nome": "Pierpaolo" }, { $set: { "nome": "Chiara" } }, function (err, data) {
+            if (!err) {
+                console.log("UpdateOne: " + data);
+            }
+            else {
+                console.log("Errore esecuzione qury: " + err.message);
+            }
+            client.close();
+        });
+    }
+    else {
+        console.log("Errore nella connessione al database: " + err.message);
+    }
+});
+
+// Delete
+mongoClient.connect(CONNECTIONSTRING, function (err, client) {
+    if (!err) {
+        let db = client.db("5B_Studenti");
+        let collection = db.collection("Studenti");
+        collection.deleteMany({ "nome": "Michele" }, function (err, data) {
+            if (!err) {
+                console.log("Delete: " + data);
             }
             else {
                 console.log("Errore esecuzione qury: " + err.message);
