8171fda54327e7ce007de56bac6a36d66db60663
Luca <l.pelissero.1360@vallauri.edu>
2021-11-17 09:54:59 +0100

    Iniziato esercizio 1 della verifica 2020

--- Mongo DB/Es 004 - verifica 2020_vallauri/server.ts
+++ Mongo DB/Es 004 - verifica 2020_vallauri/server.ts
@@ -1,29 +1,54 @@
 import * as _http from "http";
 
 import HEADERS from "./headers.json";
-import {Dispatcher} from "./dispatcher";
-let port:number = 1337;
+import { Dispatcher } from "./dispatcher";
+import * as _mongodb from "mongodb";
 
-let dispatcher :Dispatcher = new Dispatcher();
+const mongoClient = _mongodb.MongoClient;
+const CONNECTIONSTRING = "mongodb://127.0.0.1:27017";
+const DB_NAME = "5B";
+
+let port: number = 1337;
+let dispatcher: Dispatcher = new Dispatcher();
 
 // tutte le volte che arriva una richgiesta dal client parte questa funzione
 let server = _http.createServer(function (req, res) {
     dispatcher.dispatch(req, res);
 });
 server.listen(port);
-console.log("Server in ascolto sulla porta "+ port);
+console.log("Server in ascolto sulla porta " + port);
 
 // registrazione dei servizi
 dispatcher.addListener("POST", "/api/servizio1", function (req, res) {
-    res.writeHead(200, HEADERS.json);
-    let nome = req["BODY"].nome;
-    res.write(JSON.stringify({"ris": nome, "id": req["GET"].id}));
-    res.end();
-})
+    let dataStart = new Date(req["BODY"].dataStart);
+    let dataEnd = new Date(req["BODY"].dataEnd);
 
-dispatcher.addListener("GET", "/api/servizio2", function (req, res) {
-    res.writeHead(200, HEADERS.json);
-    let nome = req["GET"].nome; 
-    res.write(JSON.stringify({"ris":nome}));
-    res.end();
+    mongoClient.connect(CONNECTIONSTRING, function (err, client) {
+        if (!err) {
+            let db = client.db(DB_NAME);
+            let collection = db.collection("unicorns");
+            let req = collection.find({
+                "$and": [
+                    { "$gte": { "dob": dataStart } },
+                    { "$lte": { "dob": dataEnd } }
+                ]
+            }).project({ "nome": 1, "classe": 1 }).toArray();
+            req.then(function (data) {
+                res.writeHead(200, HEADERS.json);
+                res.write(JSON.stringify(data));
+                res.end();
+            })
+            req.catch(function (err) {
+                res.writeHead(200, HEADERS.json);
+                res.write("Errore esecuzione query: " + err.message);
+                res.end();
+            })
+            req.finally(function () {
+                client.close();
+            })
+        }
+        else {
+            console.log("Errore nella connessione al database: " + err.message);
+        }
+    });
 })
--- Mongo DB/Es 004 - verifica 2020_vallauri/static/index.js
+++ Mongo DB/Es 004 - verifica 2020_vallauri/static/index.js
@@ -1,18 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $("#btnInvia").on("click", function() {
-        let request = inviaRichiesta("post", "/api/servizio1?id=5", { "nome": "pippo" });
+    $("#btnInvia").on("click", function () {
+        let dataStart = $("#dataStart").val();
+        let dataEnd = $("#dataEnd").val();
+        let request = inviaRichiesta("post", "/api/servizio1", { "dataStart": dataStart, "dataEnd": dataEnd });
         request.fail(errore);
         request.done(function(data) {
-            alert(JSON.stringify(data));
-        });
-    });
-
-    $("#btnInvia2").on("click", function() {
-        let request = inviaRichiesta("get", "/api/servizio2", { "nome": "pippo" });
-        request.fail(errore);
-        request.done(function(data) {
-            alert(JSON.stringify(data));
+            alert(JSON.stringify(data))
         });
     });
 });
