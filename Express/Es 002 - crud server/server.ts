26cea0de73758177e03af1f9fc90405b76ce5f50
Luca <l.pelissero.1360@vallauri.edu>
2021-12-15 09:53:20 +0100

    visualizzazione nella tabella dei record delle collection

--- Express/Es 002 - crud server/server.ts
+++ Express/Es 002 - crud server/server.ts
@@ -16,19 +16,19 @@ let app = express();
 let server = http.createServer(app);
 
 server.listen(port, () => {
-    console.log("Server in ascolto sulla porta " + port)
-    init();
+   console.log("Server in ascolto sulla porta " + port)
+   init();
 });
 
 let paginaErrore = "";
 function init() {
-    fs.readFile("./static/error.html", (err, data) => {
-        if (!err) {
-            paginaErrore = data.toString();
-        } else {
-            paginaErrore = "<h2>Risorsa non trovata</h2>";
-        }
-    })
+   fs.readFile("./static/error.html", (err, data) => {
+      if (!err) {
+         paginaErrore = data.toString();
+      } else {
+         paginaErrore = "<h2>Risorsa non trovata</h2>";
+      }
+   })
 }
 
 // *********************************************************************
@@ -36,8 +36,8 @@ function init() {
 // *********************************************************************
 // 1.log
 app.use("/", (req, res, next) => {
-    console.log("----> " + req.method + ":" + req.originalUrl);
-    next();
+   console.log("----> " + req.method + ":" + req.originalUrl);
+   next();
 })
 
 // 2.static route
@@ -49,103 +49,106 @@ app.use("/", body_parser.urlencoded({ extended: true }))
 
 // 4.log dei parametri
 app.use("/", (req, res, next) => {
-    if (Object.keys(req.body).length > 0) {
-        console.log("Parametri GET: ", req.query)
-    }
-    if (Object.keys(req.body).length > 0) {
-        console.log("Parametri BODY: ", req.body)
-    }
-    next();
+   if (Object.keys(req.body).length > 0) {
+      console.log("Parametri GET: ", req.query)
+   }
+   if (Object.keys(req.body).length > 0) {
+      console.log("Parametri BODY: ", req.body)
+   }
+   next();
 })
 
 // *********************************************************************
 //              elenco delle routes di risposta al client
 // *********************************************************************
-// 5.listener di risposta al client
-// route della creazione della connessione
+// middleware di apertura della connessione
 app.use("/", (req, res, next) => {
-    mongoClient.connect(CONNECTIONSTRING, (err, client) => {
-        if (err) {
-            res.status(503).send("DB connection error");
-        }
-        else {
-            console.log("Connessione riuscita");
-            req["client"] = client; // creare un nuovo campo di req
-            next();
-        }
-    })
+   mongoClient.connect(CONNECTIONSTRING, (err, client) => {
+      if (err) {
+         res.status(503).send("DB connection error");
+      }
+      else {
+         console.log("Connessione riuscita");
+         req["client"] = client; // creare un nuovo campo di req
+         next();
+      }
+   })
 })
+
+// lettura delle collezioni preseti nel db
 app.get("/api/getCollections", (req, res, next) => {
-    let db = req["client"].db(DB_NAME) as _mongodb.Db;
-    let request = db.listCollections().toArray();
-    request.then(function (data) {
-        res.send(data);
-    })
-    request.catch(function (err) {
-        res.status(503).send("errore nella sintassi della query")
-    })
-    request.finally(function () {
-        req["client"].close();
-    })
+   let db = req["client"].db(DB_NAME) as _mongodb.Db;
+   let collection = db.listCollections();
+   let request = collection.toArray()
+   request.then(function (data) {
+      res.send(data);
+   })
+   request.catch(function (err) {
+      res.status(503).send("errore nella sintassi della query")
+   })
+   request.finally(function () {
+      req["client"].close();
+   })
 })
 
-// 6.listener
-app.patch("/api/risorsa2", (req, res, next) => {
-    let unicorn = req.query.nome;
-    let incVampires = req.body.vampires;
-    if (unicorn) {
-        let db = req["client"].db(DB_NAME) as _mongodb.Db;
-        let collection = db.collection("unicorns");
-        let request = collection.updateOne({ name: unicorn }, { $inc: { vampires: incVampires } });
-        request.then(function (data) {
-            res.send(data);
-        })
-        request.catch(function (err) {
-            res.status(503).send("errore nella sintassi della query")
-        })
-        request.finally(function () {
-            req["client"].close();
-        })
-    }
-    else {
-        res.status(400).send("Parametro mancante: UnicornName o incVampires");
-        req["client"].close();
-    }
+// middleware di intercettazione dei paramtri
+let currentCollection = "";
+let id = "";
+app.use("/api/:collection/:id?", (req, res, next) => {
+   currentCollection = req.params.collection;
+   id = req.params.id;
+   next();
 })
+// app.use("/api/:collection/:id", (req, res, next) => {
+//     id = req.params.id;
+//     next();
+// })
 
-// 7.listener
-app.get("/api/risorsa3/:gender/:hair", (req, res, next) => {
-    let gender = req.params.gender;
-    let hair = req.params.hair;
-
-    // la if sui paramtri in questo caso non serve perchè se non i sono non entra nella route
-    let db = req["client"].db(DB_NAME) as _mongodb.Db;
-    let collection = db.collection("unicorns");
-    let request = collection.find({ $and: [{ gender: gender }, { hair: hair }] }).toArray();
-    request.then(function (data) {
-        res.send(data);
-    })
-    request.catch(function (err) {
-        res.status(503).send("errore nella sintassi della query")
-    })
-    request.finally(function () {
-        req["client"].close();
-    })
+// listener specifici:
+// get (si mette in ascolto di qualsiasi richiesta get)
+app.get("/api/*", (req, res, next) => {
+   let db = req["client"].db(DB_NAME) as _mongodb.Db;
+   let collection = db.collection(currentCollection);
+   if (!id) {
+      let request = collection.find().toArray();
+      request.then(function (data) {
+         res.send(data);
+      })
+      request.catch(function (err) {
+         res.status(503).send("errore nella sintassi della query")
+      })
+      request.finally(function () {
+         req["client"].close();
+      })
+   }
+   else {
+      let oId = new _mongodb.ObjectId(id)
+      let request = collection.find({ "_id": oId }).toArray();
+      request.then(function (data) {
+         res.send(data);
+      })
+      request.catch(function (err) {
+         res.status(503).send("errore nella sintassi della query")
+      })
+      request.finally(function () {
+         req["client"].close();
+      })
+   }
 })
 
 // *********************************************************************
 // default route (risorsa non trovata) e routes di gestione degli errori
 // *********************************************************************
 app.use("/", function (req, res, next) {
-    res.status(404);
-    if (req.originalUrl.startsWith("/api/")) {
-        res.send("Servizio non trovato ");
-    } else {
-        res.send(paginaErrore);
-    }
+   res.status(404);
+   if (req.originalUrl.startsWith("/api/")) {
+      res.send("Servizio non trovato ");
+   } else {
+      res.send(paginaErrore);
+   }
 });
 
 // route di gestione errori
 app.use("/", (err, req, res, next) => {
-    console.log("Errore codice server", err.message);
+   console.log("Errore codice server", err.message);
 })
--- Express/Es 002 - crud server/static/index.js
+++ Express/Es 002 - crud server/static/index.js
@@ -1,14 +1,16 @@
 "use strict"
 
 $(document).ready(function () {
-
+    let filters = $(".card").first();
     let divIntestazione = $("#divIntestazione")
     let divCollections = $("#divCollections")
     let table = $("#mainTable")
     let divDettagli = $("#divDettagli")
     let currentCollection = "";
 
-    let request = inviaRichiesta("get", "api/getCollections");
+    filters.hide();
+
+    let request = inviaRichiesta("get", "/api/getCollections");
     request.fail(errore)
     request.done(function (collections) {
         let label = divCollections.children("label");
@@ -22,7 +24,41 @@ $(document).ready(function () {
         }
         label.remove();
     })
+
     divCollections.on("click", "input[type=radio]", function () {
-        
+        currentCollection = $(this).val();
+        let request = inviaRichiesta("GET", "/api/" + currentCollection)
+        request.fail(errore)
+        request.done(function (data) {
+            divIntestazione.find("strong").eq(0).text(currentCollection)
+            divIntestazione.find("strong").eq(0).text(data.length)
+            if (currentCollection == "unicorns") {
+                filters.show();
+            }
+            else {
+                filters.hide();
+            }
+            table.children("tbody").empty()
+            for (const item of data) {
+                let tr = $("<tr>").appendTo(table.children("tbody"))
+                let td = $("<td>").appendTo(tr).text(item._id).prop("id", item._id).on("click", visualizzaDettagli)
+                td = $("<td>").appendTo(tr).text(item.name).prop("id", item._id).on("click", visualizzaDettagli)
+                for (let i = 0; i < 3; i++) {
+                    $("<div>").appendTo(td);
+                }
+            }
+        })
     })
+
+    function visualizzaDettagli() {
+        let request = inviaRichiesta("GET", "/api/" + currentCollection + "/" + $(this).prop("id"))
+        request.fail(errore);
+        request.done(function (data) {
+            let content = "";
+            for (const key in data[0]) {
+                content += "<strong>" + key + ":</strong> " + data[0][key] + "<br>";
+                divDettagli.html(content);
+            }
+        })
+    }
 });
