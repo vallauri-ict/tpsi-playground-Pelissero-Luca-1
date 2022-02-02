1304f4063897e7a2908c996069642f1b1dfe6481
Luca <l.pelissero.1360@vallauri.edu>
2022-02-02 09:21:27 +0100

    Some server error fixed

--- Express/Es 003 - upload/server.ts
+++ Express/Es 003 - upload/server.ts
@@ -1,153 +1,186 @@
-//#region import
-import * as http from "http";
-import * as fs from "fs";
-import * as bodyParser from "body-parser";
 import express from "express";
+import * as fs from "fs";
+import * as http from "http";
+import * as body_parser from "body-parser";
 import * as mongodb from "mongodb";
+const fileupload = require('express-fileupload');
 import cors from "cors";
-import fileUpload, { UploadedFile } from "express-fileupload";
-//#endregion
+import { UploadedFile } from "express-fileupload";
 
-//#region mongoDB
+
 const mongoClient = mongodb.MongoClient;
-const CONNECTION_STRING =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://admin:admin@cluster0.eawws.mongodb.net/5B?retryWrites=true&w=majority";
-// const CONNECTION_STRING = "mongodb://127.0.0.1:27017";
-// const CONNECTION_STRING =
-//   "mongodb+srv://admin:admin@cluster0.niwz6.mongodb.net/5B?retryWrites=true&w=majority";
+const CONNECTION_STRING = process.env.MONGODB_URI || "mongodb+srv://edoardo:aba@cluster0.7z0jw.mongodb.net/5B?retryWrites=true&w=majority";
+/*const CONNECTION_STRING =
+  "mongodb://admin:admin@cluster0-shard-00-00.zarz7.mongodb.net:27017,cluster0-shard-00-01.zarz7.mongodb.net:27017,cluster0-shard-00-02.zarz7.mongodb.net:27017/test?replicaSet=atlas-bgntwo-shard-0&ssl=true&authSource=admin";*/
 const DB_NAME = "5B";
-//#endregion
 
-const PORT: number = parseInt(process.env.PORT) || 1337;
-const app = express();
 
-const server = http.createServer(app);
+let port = parseInt(process.env.PORT) || 1337;
+let app = express();
 
-server.listen(PORT, () => {
-  console.log(`Server in ascolto sulla porta: ${PORT}`);
+let server = http.createServer(app);
+
+server.listen(port, function () {
+  console.log("Server in ascolto sulla porta " + port)
+
   init();
 });
 
-let paginaErrore = "";
-function init() {
-  fs.readFile("./static/error.html", (err, data) => {
-    if (!err) paginaErrore = data.toString();
-    else paginaErrore = "<h2>Risorsa non trovata</h2>";
-  });
-}
-
-const whitelist = [
-  "http://localhost:4200",
-  "http://localhost:1337"
-];
-
-/*  ******************************************
-    //  elenco delle routes middleware
-    ****************************************** */
-
-//  1. Log
-app.use("/", (req, res, next) => {
-  console.log("---> ", req.method + ": " + req.originalUrl);
-  next();
-});
-
-//  2. Static route
-app.use("/", express.static("./static")); //  next fa in automatico
-
-//  3. Route lettura paramentri post
-app.use("/", bodyParser.json());
-app.use("/", bodyParser.urlencoded({ extended: true }));
-
-//  4. Log dei parametri
-app.use("/", (req, res, next) => {
-  if (Object.keys(req.query).length > 0) console.log("GET --->", req.query);
-  if (Object.keys(req.body).length > 0) console.log("BODY --->", req.body);
-  next();
-});
-
-//  5. Middleware cors
+const whitelist = ["http://localhost:4200", "http://localhost:1337"];
 const corsOptions = {
   origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
+    if (!origin)
+      return callback(null, true);
     if (whitelist.indexOf(origin) === -1) {
-      var msg =
-        "The CORS policy for this site does not " +
-        "allow access from the specified Origin.";
+      var msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
       return callback(new Error(msg), false);
-    } else return callback(null, true);
+    }
+    else
+      return callback(null, true);
   },
-  credentials: true,
+  credentials: true
 };
 app.use("/", cors(corsOptions));
 
-// 6. fileupload
-app.use(fileUpload({
+app.use(fileupload({
   "limits ": { "fileSize ": (10 * 1024 * 1024) } // 10 MB
-}));
+ }));
+ 
 
 
-// Connessione al DB
+let paginaErrore = "";
+function init() {
+  fs.readFile("./static/error.html", function (err, data) {
+    if (!err) {
+      paginaErrore = data.toString();
+    }
+    else {
+      paginaErrore = "<h2>Risorsa non trovata</h2>";
+    }
+  });
+}
+
+
+//****************************************************************
+//elenco delle routes di tipo middleware
+//****************************************************************
+// 1.log 
+app.use("/", function (req, res, next) {
+  console.log("---->" + req.method + ":" + req.originalUrl);
+  next();
+});
+
+// 2.static route
+//il next lo fa automaticamente quando non trova la risorsa
+app.use("/", express.static("./static"));
+
+// 3.route lettura parametri post
+app.use("/", body_parser.json());
+app.use("/", body_parser.urlencoded({ "extended": true }));
+
+// 4.log parametri
+app.use("/", function (req, res, next) {
+  if (Object.keys(req.query).length > 0) {
+    console.log("Parametri GET: ", req.query);
+  }
+  if (Object.keys(req.body).length > 0) {
+    console.log("Parametri BODY: ", req.body);
+  }
+  next();
+})
+
+
+//****************************************************************
+//elenco delle routes di risposta al client
+//****************************************************************
+// middleware di apertura della connessione
 app.use("/", (req, res, next) => {
   mongoClient.connect(CONNECTION_STRING, (err, client) => {
-    if (err) res.status(503).send("DB connection error");
-    else {
-      req["client"] = client;
+    if (err) {
+      res.status(503).send("Db connection error");
+    } else {
+      console.log("Connection made");
+      req["client"] = client;//salva il client così evito si aprorla tutte le volte
       next();
     }
   });
 });
 
-/*  ******************************************
-    elenco delle routes di risposta al client
-    ****************************************** */
 
-//  middleware di intercettazione dei parametri
-let currentCollection: string = "";
-
-//  listener specifici
+// listener specifici: 
 
 app.get("/api/images", (req, res, next) => {
-  if (currentCollection) {
-    let db = req["client"].db(DB_NAME) as mongodb.Db;
-    let collection = db.collection("images");
-    collection
-      .find().toArray()
-      .then((data) => res.send(data))
-      .catch((err) => res.status(503).send("QUERY: Syntax error"))
-      .finally(() => req["client"].close());
-  }
-});
+  let db = req["client"].db(DB_NAME) as mongodb.Db;
+  let collection = db.collection('images');
+  let request = collection.find().toArray();
+  request.then((data) => {
+    res.send(data);
+  });
+  request.catch((err) => {
+    res.status(503).send("Sintax error in the query");
+  });
+  request.finally(() => {
+    req["client"].close();
+  });
+})
 
-app.get("/api/uploadBinary", (req, res, next) => {
-  if (!req.files || Object.keys(req.files).length == 0 || !req.body.username)
-    res.status(400).send('Manca immagine o username');
+///api/uploadBase64
+
+app.post("/api/uploadBinary", (req, res, next) => {
+
+  if (!req.files  || Object.keys(req.files).length == 0)
+    res.status(400).send('No files were uploaded');
   else {
     let _file = req.files.img as UploadedFile;
     _file.mv('./static/img/' + _file["name"], function (err) {
       if (err)
         res.status(500).json(err.message);
-      else{
-        // insert del nuovo record nel db
-        if (currentCollection) {
-          let db = req["client"].db(DB_NAME) as mongodb.Db;
-          let collection = db.collection("images");
-          let user = {"username": req.body.username, "img": _file.name}
-          collection
-            .insertOne(user)
-            .then((data) => res.send(data))
-            .catch((err) => res.status(503).send("QUERY: Syntax error"))
-            .finally(() => req["client"].close());
-        }
+      else {
+        let db = req["client"].db(DB_NAME) as mongodb.Db;
+        let collection = db.collection('images');
+        let user ={"username":req.body.username,"img":_file.name}
+        let request = collection.insertOne(user)
+        request.then((data) => {
+          res.send(data);
+        });
+        request.catch((err) => {
+          res.status(503).send("Sintax error in the query");
+        });
+        request.finally(() => {
+          req["client"].close();
+        });
       }
     })
   }
 
-});
 
-/*  ******************************************
-    default route e route di gestione degli errori
-    ****************************************** */
-app.use("/", (err, req, res, next) => {
-  console.log("**** ERRORE SERVER ***** " + err); //  da correggere
-});
+
+  // const getPageTwo = async ( page)=>{    
+  //   return await (await fetch(connection_base+page)).json();
+  // }
+
+  // const getData = async (page)=>{
+  //     const comuni = await getPageTwo(page)
+  //     console.log(comuni)
+  // }
+})
+
+
+//****************************************************************
+//file upload
+//****************************************************************
+app.use(fileupload({
+  "limits ": { "fileSize ": (10 * 1024 * 1024) } // 10 MB
+}));
+
+
+//****************************************************************
+//default route(risorse non trovate) e route di gestione degli errori
+//****************************************************************
+app.use("/", function (err, req, res, next) {
+  console.log("***************  ERRORE CODICE SERVER ", err.message, "  *****************");
+})
+
+
+
